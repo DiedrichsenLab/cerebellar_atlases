58c8763e22821d5fac9dc7dc4d31516ddff30f2d
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2021-05-11 21:42:22 -0400

    Color Lut and surfaces giftis

--- code/cereb_atlas_process.py
+++ code/cereb_atlas_process.py
@@ -4,6 +4,7 @@ import xml.etree.cElementTree as ET
 import pandas as pd
 import nibabel as nb
 import numpy as np
+import SUITPy as suit
 
 def make_atlas_list():
     """
@@ -40,7 +41,7 @@ def rgbtxt_to_lut(filename):
             "G":D[3]/255,
             "B":D[4]/255,
             "Name":D[1]})
-    L.to_csv(filename+'.lut',header=None,delimiter=' ')
+    L.to_csv(filename+'.lut',header=None,sep=' ')
 
 def read_all_atlasdescrip():
     """
@@ -120,19 +121,50 @@ def make_MDTB_contrasts():
             nb.save(img, filename)
     pass
 
-def preprocess_nifti(fname,isLabel):
+def preprocess_nifti(fname,isLabel=True):
     """
         Preprocesses (cleans) the nifti file by setting data type and intent
     """
-    nii = nb.load(os.path.join(dir,file + '.nii'))
+    nii = nb.load(fname + '.nii')
     X = nii.get_fdata()
     img = nb.Nifti1Image(X.round(0), nii.affine)
     if isLabel: 
         img.set_data_dtype('int8')
+        img.header.set_intent(1002,(),"")
+        img.header.set_slope_inter(1.0,0.0)
     else:
         img.set_data_dtype('int16')
-    print(img.get_shape())
-    nb.save(img, fname)
+    print(img.shape)
+    nb.save(img, fname + '.nii')
+
+def preprocess_all(): 
+    preprocess_nifti('atl-Buckner/atl-Buckner7_sp-MNI')
+    preprocess_nifti('atl-Buckner/atl-Buckner7_sp-SUIT')
+    preprocess_nifti('atl-Buckner/atl-Buckner17_sp-MNI')
+    preprocess_nifti('atl-Buckner/atl-Buckner17_sp-SUIT')
+    preprocess_nifti('atl-Ji/atl-Ji10_sp-MNI')
+    preprocess_nifti('atl-Ji/atl-Ji10_sp-SUIT')
+    preprocess_nifti('atl-Anatom/atl-Anatom_sp-MNI')
+    preprocess_nifti('atl-Anatom/atl-Anatom_sp-SUIT')
+    preprocess_nifti('atl-MDTB/atl-MDTB10_sp-MNI')
+    preprocess_nifti('atl-MDTB/atl-MDTB10_sp-SUIT')
+
+def map_to_surf(fname,isLabel=True):
+    """ 
+        Maps a specific label file to the SUIT flatmap
+    """ 
+    nii_name = fname + '_sp-SUIT.nii'
+    gii_name = fname + '.label.gii'
+    labeldata = suit.flatmap.vol_to_surf([nii_name],stats = 'mode')
+    lut_name = fname + '.lut'
+    D=pd.read_csv(lut_name,header=None,delimiter=' ')
+    RGBA = np.c_[D[1],D[2],D[3],np.ones((D.shape[0],))]
+    RGBA = np.vstack([np.zeros((1,4)),RGBA])
+    labels = ['???']
+    labels = labels + D[4].tolist()
+    L = suit.flatmap.make_label_gifti(labeldata,
+        label_names=labels,column_names=['label'],label_RGBA=RGBA)
+    nb.save(L,gii_name)
 
 def make_MDTB_json(): 
     """
@@ -147,9 +179,11 @@ def make_MDTB_json():
             json.dump(condict,outfile)
 
 if __name__ == "__main__":
+    map_to_surf('atl-Buckner/atl-Buckner7')
+    # preprocess_all()
     # make_atlas_list()
-    descr = read_all_atlasdescrip()
-    write_readme(descr)
+    # descr = read_all_atlasdescrip()
+    # write_readme(descr)
     # export_as_FSLatlas('Buckner','Buckner7')
     # rgbtxt_to_lut('atl-Xue10Sub1_desc-color.txt')
     # make_MDTB_json()
