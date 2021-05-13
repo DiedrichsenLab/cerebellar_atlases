b50b56af36627cf81c7512ca14e6b78fd5bd277e
Jörn Diedrichsen <joern.diedrichsen@googlemail.com>
2021-05-12 22:47:46 -0400

    Flatmaps added

--- code/cereb_atlas_process.py
+++ code/cereb_atlas_process.py
@@ -12,26 +12,32 @@ def make_atlas_list():
     """
     jsondict = {}
     jsondict['Atlas'] = []
-    jsondict['Maps'] = []
+    jsondict['ShortDesc'] = []
     jsondict['Type'] = []
-    
-    directories =  os.listdir()
+    jsondict['Maps'] = []
+    jsondict['MapDesc'] = []
+    jsondict['Type'] = []
 
+    directories =  ['atl-Anatom','atl-Buckner','atl-Xue','atl-Ji','atl-MDTB','con-MDTB']
     for name in directories:
         if name.startswith('atl-'):
             with open(name +'/atlas_description.json') as jsonfile:
                 file = json.load(jsonfile)
                 jsondict["Type"].append("Atlas")
                 jsondict["Atlas"].append(name)
+                jsondict["ShortDesc"].append(file["ShortDesc"])
                 jsondict["Maps"].append(file["Maps"])
+                jsondict["MapDesc"].append(file["MapDesc"])
         if name.startswith('con-'):
             with open(name +'/contrast_description.json') as jsonfile:
                 file = json.load(jsonfile)
                 jsondict["Type"].append("Contrast")
                 jsondict["Atlas"].append(name)
+                jsondict["ShortDesc"].append(file["ShortDesc"])
                 jsondict["Maps"].append(file["Maps"])
+                jsondict["MapDesc"].append(file["MapDesc"])
     with open('package_description.json','w') as outfile:
-        json.dump(jsondict,outfile)
+        json.dump(jsondict,outfile,indent = 5)
 
 def rgbtxt_to_lut(filename):
     D=pd.read_csv(filename,header=None,delimiter=' ')
@@ -64,14 +70,14 @@ def read_all_atlasdescrip():
 
 
 
-def write_readme(descr): 
-    with open('README.md','w') as out: 
+def write_readme(descr):
+    with open('README.md','w') as out:
         out.write("# Cerebellar Atlases\n")
         out.write("The cerebellar atlases are a collection of anatomical and functional atlases of the human cerebellum, both of parcellations and continuous maps.")
-        out.write("For every maps, we provide some the following files\n" + 
+        out.write("For every maps, we provide some the following files\n" +
         "* ..._sp-MNI.nii: volume file aligned to FNIRT MNI space\n" +
         "* ..._sp-SUIT.nii: volume file aligned to SUIT space\n" +
-        "* ..._lut: Color and label lookup table for parcellations\n" + 
+        "* ..._lut: Color and label lookup table for parcellations\n" +
         "* ....gii: Data projected to surface-based representation of the cerebellum (Diedrichsen & Zotow, 2015).\n")
         out.write("The atlases can also be viewed online using out cerebellar atlas tool for quick reference!\n\n")
         for d in descr:
@@ -80,7 +86,7 @@ def write_readme(descr):
             for i,f in enumerate(d["Maps"]):
                 out.write("* " + f + ":    " + d["MapDesc"][i] +"\n")
             out.write("\nReferences and Links:\n")
-            for ref in d["ReferencesAndLinks"]: 
+            for ref in d["ReferencesAndLinks"]:
                 out.write("* " + ref + "\n")
             out.write("\n\n")
         out.write("## Reference and Licence\n")
@@ -95,12 +101,12 @@ def export_as_FSLatlas(name = None, atlas = None):
     tree = ET.ElementTree(root)
     tree.write("filename.xml")
 
-def make_MDTB_contrasts(): 
+def make_MDTB_contrasts():
     """
         Generates the MDTB contrasts from the atlas package with correct naming
     """
     D = pd.read_csv('../MDTB_maps/contrast_list.csv')
-    for at in ['SUIT']: 
+    for at in ['SUIT']:
         for c in range(50):
             images = D[D.ConNo == c+1]
             num_img = images.Contrast.shape[0]
@@ -111,7 +117,7 @@ def make_MDTB_contrasts():
                 nii = nb.load(os.path.join(dir,file + '.nii'))
                 X.append(nii.get_fdata())
             pass
-            if num_img==2: 
+            if num_img==2:
                 Y = (X[0]+X[1])/2
             else:
                 Y = X[0]
@@ -128,7 +134,7 @@ def preprocess_nifti(fname,isLabel=True):
     nii = nb.load(fname + '.nii')
     X = nii.get_fdata()
     img = nb.Nifti1Image(X.round(0), nii.affine)
-    if isLabel: 
+    if isLabel:
         img.set_data_dtype('int8')
         img.header.set_intent(1002,(),"")
         img.header.set_slope_inter(1.0,0.0)
@@ -137,7 +143,7 @@ def preprocess_nifti(fname,isLabel=True):
     print(img.shape)
     nb.save(img, fname + '.nii')
 
-def preprocess_all(): 
+def preprocess_all():
     preprocess_nifti('atl-Buckner/atl-Buckner7_sp-MNI')
     preprocess_nifti('atl-Buckner/atl-Buckner7_sp-SUIT')
     preprocess_nifti('atl-Buckner/atl-Buckner17_sp-MNI')
@@ -150,36 +156,57 @@ def preprocess_all():
     preprocess_nifti('atl-MDTB/atl-MDTB10_sp-SUIT')
 
 def map_to_surf(fname,isLabel=True):
-    """ 
+    """
         Maps a specific label file to the SUIT flatmap
-    """ 
+    """
     nii_name = fname + '_sp-SUIT.nii'
-    gii_name = fname + '.label.gii'
-    labeldata = suit.flatmap.vol_to_surf([nii_name],stats = 'mode')
-    lut_name = fname + '.lut'
-    D=pd.read_csv(lut_name,header=None,delimiter=' ')
-    RGBA = np.c_[D[1],D[2],D[3],np.ones((D.shape[0],))]
-    RGBA = np.vstack([np.zeros((1,4)),RGBA])
-    labels = ['???']
-    labels = labels + D[4].tolist()
-    L = suit.flatmap.make_label_gifti(labeldata,
-        label_names=labels,column_names=['label'],label_RGBA=RGBA)
+    if isLabel:
+        gii_name = fname + '.label.gii'
+        labeldata = suit.flatmap.vol_to_surf([nii_name],stats = 'mode')
+        lut_name = fname + '.lut'
+        D=pd.read_csv(lut_name,header=None,delimiter=' ')
+        RGBA = np.c_[D[1],D[2],D[3],np.ones((D.shape[0],))]
+        RGBA = np.vstack([np.zeros((1,4)),RGBA])
+        labels = ['None']
+        labels = labels + D[4].tolist()
+        L = suit.flatmap.make_label_gifti(labeldata,
+            label_names=labels,column_names=['label'],label_RGBA=RGBA)
+    else:
+        pass
     nb.save(L,gii_name)
 
-def make_MDTB_json(): 
+def all_maps_to_surf():
+    with open('package_description.json') as jsonfile:
+        file = json.load(jsonfile)
+        for i,name in enumerate(file['Atlas']):
+            if file['Type'][i]=='Atlas':
+                map = file['Maps'][i]
+                for m in map:
+                    map_to_surf(os.path.join(name,'atl-' + m),True)
+            if file['Type'][i]=='Contrast':
+                map = file['Maps'][i]
+                for m in map:
+                    map_to_surf(os.path.join(name,'atl-' + m),False)
+    pass
+
+
+def make_MDTB_json():
     """
         Generates the MDTB json file from the csv
     """
     D = pd.read_csv('con-MDTB/contrast_description.csv')
     with open('atl-MDTB/atlas_description.json') as jsonfile:
         condict = json.load(jsonfile)
-        condict['Maps']=D['Contrast'].tolist()
+        condict['ShortDesc'] = "Multi-domain task battery (MDTB) contrast maps: King et al. (2019)"
+        condict['LongDesc'] = "King et al. (2019) provided an extensive characterization of the functional organization of the cerebellum of 24 healthy, young participants. The contast are for for 47 task conditions, accounted for the activity caused by left hand, right hand, and eye movements. All maps are relative to the mean activitiy across all tasks."
         condict['MapDesc']=D['Description'].tolist()
-        with open('con-MDTB/contrast_description_new.json','w') as outfile:
-            json.dump(condict,outfile)
+        condict['Maps'] = []
+        for i,d in D.iterrows():
+            condict['Maps'].append(f'MDTB{d.ConNo:02}{d.Contrast}')
+        with open('con-MDTB/contrast_description.json','w') as outfile:
+            json.dump(condict,outfile,indent = 4)
 
 if __name__ == "__main__":
-    map_to_surf('atl-Buckner/atl-Buckner7')
     # preprocess_all()
     # make_atlas_list()
     # descr = read_all_atlasdescrip()
@@ -187,4 +214,6 @@ if __name__ == "__main__":
     # export_as_FSLatlas('Buckner','Buckner7')
     # rgbtxt_to_lut('atl-Xue10Sub1_desc-color.txt')
     # make_MDTB_json()
-    pass 
+    # all_maps_to_surf()
+    map_to_surf('atl-MDTB/atl-MDTB10',isLabel = True)
+    pass
